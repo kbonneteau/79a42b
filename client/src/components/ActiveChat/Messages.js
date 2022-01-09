fa1883eaf8eb379b27b951cb124dac6f08a8d360
Kayla Bonneteau <k.bonneteau@gmail.com>
2022-01-08 22:05:13 -0800

    Copy and sort messages before displaying.

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
+  const sortedMessages = useMemo(
+    () =>
+    // Create copy of messages to avoid mutating messages.
+    messages
+        .slice()
+        .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt)),
+    [messages]
+  );
 
   return (
     <Box>
-      {messages.map((message) => {
+      {sortedMessages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
