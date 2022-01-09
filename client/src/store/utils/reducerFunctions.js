325c530745fa8e2f290edf94656f6c5e003da0f2
Kayla Bonneteau <k.bonneteau@gmail.com>
2022-01-08 20:12:58 -0800

    Fix state mutation for new conversations.

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -71,10 +71,13 @@ export const addSearchedUsersToStore = (state, users) => {
 export const addNewConvoToStore = (state, recipientId, message) => {
   return state.map((convo) => {
     if (convo.otherUser.id === recipientId) {
-      convo.id = message.conversationId;
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
+      // spread conversation properties to circumvent mutating state
+      return {
+        ...convo,
+        id: message.conversationId,
+        messages: [...convo.messages, message],
+        latestMessageText: message.text
+      };
     } else {
       return convo;
     }
